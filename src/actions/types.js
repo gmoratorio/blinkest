668ec1a7c0e11ee2a2dc6534a96bc336ec758a3b
gmoratorio <moratorio.guillermo@gmail.com>
2018-02-24 23:10:43 -0700

    GH-4 -- added search functionality

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,3 +1,4 @@
 export const RESULTS_SORTED = 'results_sorted';
 export const VEHICLE_POSTS_READY = 'vehicle_posts_ready';
 export const UPDATED_DIMENSIONS = 'updated_dimensions';
+export const UPDATED_SEARCH_TEXT = 'updated_search_text';
--- src/reducers/TableReducer.js
+++ src/reducers/TableReducer.js
@@ -1,15 +1,60 @@
 import _ from 'lodash';
 
 import {
-    RESULTS_SORTED, VEHICLE_POSTS_READY
+    RESULTS_SORTED,
+    VEHICLE_POSTS_READY,
+    UPDATED_SEARCH_TEXT
 } from '../actions/types';
 
 import {SORT_STYLE, POST_SPECS} from '../constants';
 
 const INITIAL_STATE = {
     vehiclePosts: [],
+    filteredPosts: [],
     sortKey: POST_SPECS.CREATED_AT,
     sortStyle: SORT_STYLE.ASC,
+    searchText: null
+};
+
+const sortPosts = ({posts, sortKey, sortOrder}) => {
+
+    let sortedPosts = _.sortBy(posts, post => {
+        return post[sortKey];
+    });
+
+    if (sortOrder === SORT_STYLE.DESC) {
+        _.reverse(sortedPosts);
+    }
+
+    return sortedPosts;
+};
+
+const filterPosts = ({posts, searchText}) => {
+    if (!searchText) {
+        return posts;
+    }
+
+    const searchArray = _.split(searchText, ' ');
+
+    let filteredPosts = _.filter(posts, post => {
+        const prunedPost = _.pick(post, [POST_SPECS.YEAR, POST_SPECS.MAKE, POST_SPECS.MODEL]);
+
+        return _.every(searchArray, (searchEntry) => {
+            return !!_.find(prunedPost, (value) =>{
+                const attemptedNumberConversion = _.toNumber(searchEntry);
+
+                if(!_.isNaN(attemptedNumberConversion) && _.isNumber(attemptedNumberConversion)){
+                    return attemptedNumberConversion === value;
+                } else {
+                    return _.includes(value, searchEntry);
+                }
+            });
+        });
+
+    });
+
+
+    return filteredPosts;
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -17,29 +62,27 @@ export default (state = INITIAL_STATE, action) => {
 
     switch (type) {
         case VEHICLE_POSTS_READY:
-            return {...state, vehiclePosts: payload.vehiclePosts};
+            return {...state, vehiclePosts: payload.vehiclePosts, filteredPosts: payload.vehiclePosts};
 
         case RESULTS_SORTED:
             const {sortKey, sortStyle, vehiclePosts} = state;
-
             const {newSortKey} = payload;
 
             let nextSortStyle = SORT_STYLE.ASC;
-
             if (newSortKey === sortKey) {
                 nextSortStyle = sortStyle === SORT_STYLE.ASC ? SORT_STYLE.DESC : SORT_STYLE.ASC;
             }
 
-            let sortedPosts = _.sortBy(vehiclePosts, post =>{
-               return post[newSortKey];
-            });
-
-            if(nextSortStyle === SORT_STYLE.DESC){
-                _.reverse(sortedPosts);
-            }
+            let sortedPosts = sortPosts({posts: vehiclePosts, sortKey: newSortKey, sortOrder: nextSortStyle});
 
             return {...state, sortKey: newSortKey, sortStyle: nextSortStyle, vehiclePosts: sortedPosts};
 
+        case UPDATED_SEARCH_TEXT:
+            const {newSearchText} = payload;
+
+            let filteredPosts = filterPosts({posts: state.vehiclePosts, searchText: newSearchText});
+
+            return {...state, searchText: newSearchText, filteredPosts: filteredPosts}
 
         default:
             return state;
